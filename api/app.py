72670b453a1a81fbb27465907f023cefa7051235
shashankanil <shashankmay6@gmail.com>
2023-09-20 23:19:09 +0530

    prompt changes

--- api/app.py
+++ api/app.py
@@ -15,7 +15,7 @@ import boto3
 import openai
 
 openai.api_key = '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'
-system_prompt = "Given a music prompt describing the mood, theme, and style of a song or album, generate an image prompt that represents the album cover for this music. The image should capture the essence of the music, its emotions, and the overall vibe it conveys. Be creative and imaginative in your image prompt generation.[prompt should be only in 25 words]"
+system_prompt = "Given a music prompt describing the mood, theme, and style of a song or album, generate an image prompt that represents the album cover for this music. The image should capture the essence of the music, its emotions, and the overall vibe it conveys. Be creative and imaginative in your image prompt generation.[prompt should be only in 25 words] prompt:"
 
 load_dotenv()
 REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
@@ -108,12 +108,8 @@ def fetch_full_song():
     combined_file_path = combine_audio_files(audio_files_links)
     chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
       {
-        "role": "system",
-        "content": system_prompt
-      },
-      {
         "role": "user",
-        "content": prompt
+        "content": system_prompt+prompt
       }
     ]);
     print(chat_completion["choices"][0]["message"]["content"])
@@ -146,12 +142,8 @@ def fetch_song_from_emotion():
     combined_file_path = combine_audio_files(audio_files_links)
     chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
       {
-        "role": "system",
-        "content": system_prompt
-      },
-      {
         "role": "user",
-        "content": prompt
+        "content": system_prompt+prompt
       }
     ]);
     print(chat_completion["choices"][0]["message"]["content"])
